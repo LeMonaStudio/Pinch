6884bd250d1cf4ff4a9b3403f33651aec825aa6e
Musa Lawrence Ilemona <musa.lawrence93@gmail.com>
2023-05-19 00:16:00 +0100

    Implemented the Image double tap to zoom feature

--- Pinch/Screens/ContentView.swift
+++ Pinch/Screens/ContentView.swift
@@ -8,14 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    //MARK: - PROPERTY
+    @State private var isAnimating: Bool = false
+    @State private var imageScale: CGFloat = 1
+    
+    //MARK: - BODY
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        NavigationView {
+            ZStack {
+                //MARK: - PAGE IMAGE
+                Image("magazine-front-cover")
+                    .resizable()
+                    .scaledToFit()
+                    .cornerRadius(10)
+                    .padding()
+                    .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
+                    .opacity(isAnimating ? 1 : 0)
+                    .animation(.linear(duration: 1), value: isAnimating)
+                    .scaleEffect(imageScale)
+                    .onTapGesture(count: 2) {
+                        if imageScale == 1 {
+                            withAnimation(.spring()){
+                                imageScale = 5
+                            }
+                        } else {
+                            withAnimation(.easeOut(duration: 0.5)){
+                                imageScale = 1
+                            }
+                        }
+                    }
+                    
+                    
+            } //: ZSTACK
+            .navigationBarTitle("Pinch & Zoom", displayMode: .inline)
+            .onAppear{
+                isAnimating.toggle()
+            }
+        } //: NAVIGATION
+        .navigationViewStyle(.stack)
     }
 }
 
