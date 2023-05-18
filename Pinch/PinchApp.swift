4b96a691c4464810256a592eb7a00927971e8ca1
Musa Lawrence Ilemona <musa.lawrence93@gmail.com>
2023-05-18 16:10:11 +0100

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Musa Lawrence on 5/18/23.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
