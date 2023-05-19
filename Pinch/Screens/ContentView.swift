10100b0299e8bbbdc15c266005ef270ba2cd185c
Musa Lawrence Ilemona <musa.lawrence93@gmail.com>
2023-05-19 01:15:10 +0100

    Implemented the image drag gesture

--- Pinch/Screens/ContentView.swift
+++ Pinch/Screens/ContentView.swift
@@ -11,6 +11,15 @@ struct ContentView: View {
     //MARK: - PROPERTY
     @State private var isAnimating: Bool = false
     @State private var imageScale: CGFloat = 1
+    @State private var imageOffset: CGSize = .zero
+    
+    //MARK: - FUNCTION
+    func resetImageState() {
+        withAnimation(.spring()){
+            imageScale = 1
+            imageOffset = .zero
+        }
+    }
     
     //MARK: - BODY
     var body: some View {
@@ -24,20 +33,36 @@ struct ContentView: View {
                     .padding()
                     .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
                     .opacity(isAnimating ? 1 : 0)
+                    .offset(x: imageOffset.width, y: imageOffset.height)
                     .animation(.linear(duration: 1), value: isAnimating)
                     .scaleEffect(imageScale)
+                    //MARK: - 1. TAP GESTURE
                     .onTapGesture(count: 2) {
                         if imageScale == 1 {
                             withAnimation(.spring()){
                                 imageScale = 5
                             }
                         } else {
-                            withAnimation(.easeOut(duration: 0.5)){
-                                imageScale = 1
-                            }
+                            resetImageState()
                         }
                     }
-                    
+                    //MARK: - 2. DRAG GESTURE
+                    .gesture(
+                        DragGesture()
+                            .onChanged({ gesture in
+                                withAnimation(.linear(duration: 1)){
+                                    imageOffset = gesture.translation
+                                }
+                            })
+                            .onEnded({ gesture in
+                                if imageScale <= 1 {
+                                    resetImageState()   
+                                } else {
+                                    imageOffset = gesture.translation
+                                    imageScale = 5
+                                }
+                            })
+                    )
                     
             } //: ZSTACK
             .navigationBarTitle("Pinch & Zoom", displayMode: .inline)
